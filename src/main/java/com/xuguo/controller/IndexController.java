28a638f0af2f50dd33787081e006f48fe3172d66
Xu Guo <xguo@SPs-MacBook-Pro-28.local>
2018-07-03 00:24:37 -0700

    add client

--- src/main/java/com/xuguo/controller/IndexController.java
+++ src/main/java/com/xuguo/controller/IndexController.java
@@ -7,14 +7,6 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class IndexController {
 
-    @RequestMapping("/")
-    public ModelAndView root(){
-        ModelAndView mav = new ModelAndView();
-        mav.setViewName("index");
-        mav.addObject("title","Gallery");
-        return mav;
-    }
-
     @RequestMapping("/login")
     public String login(){
         return "/login";
